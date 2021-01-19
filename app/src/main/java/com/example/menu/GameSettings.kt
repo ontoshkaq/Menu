fb9dd2043529d52569a8362e7f3af60b6e5356dc
Ontoshkaq <freeska2019@mail.ru>
2021-01-19 11:18:58 +0700

    Добавили обработку нажатия для перехода

--- app/src/main/java/com/example/menu/GameSettings.kt
+++ app/src/main/java/com/example/menu/GameSettings.kt
@@ -3,7 +3,7 @@ package com.example.menu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Options : AppCompatActivity() {
+class GameSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
--- app/src/main/java/com/example/menu/MainMenu.kt
+++ app/src/main/java/com/example/menu/MainMenu.kt
@@ -1,15 +1,48 @@
 package com.example.menu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val
+        //Continue
+        val continue_game = findViewById<Button>(R.id.b_continue)
+        continue_game.setOnClickListener{
+            val perehod = Intent(this, Continue::class.java )
+            startActivity(perehod)
+        }
+
+        //New Game
+        val new_game = findViewById<Button>(R.id.b_newgame)
+        new_game.setOnClickListener{
+            val peregod = Intent(this, NewGame::class.java )
+            startActivity(peregod)
+
+
+        }
+
+
+        //LoadGame
+        val load_game = findViewById<Button>(R.id.b_loadgame)
+        load_game.setOnClickListener{
+            val perehod = Intent(this, LoadGame::class.java )
+            startActivity(perehod)
+
+
+
+    //Settings
+            val game_settings = findViewById<Button>(R.id.b_settings)
+            new_game.setOnClickListener{
+                val perehod = Intent(this, GameSettings::class.java )
+                startActivity(perehod)
+
 //
-//        startActivity()
+//
+//
     }
-}
+}}}
